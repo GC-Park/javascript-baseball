d8d2a5173e7535b663a5657246837d1808cb26dc
GC-Park <kuyt3122@naver.com>
2022-12-11 16:41:52 +0900

    feat:컴퓨터가 1~9까지의 서로 다른 수로 이루어진 3자리 수 생성기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+const MissionUtils = require('@woowacourse/mission-utils')
 class App {
-  play() {}
+  #computerInput;
+
+  constructor(){
+    this.#computerInput=[]
+  }
+
+  play() {
+    this.#computerInput=this.makeRandomNumber()
+  }
+
+  makeRandomNumber(){
+    const computer = [];
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+    return computer
+  }
 }
 
+const app = new App();
+app.play();
 module.exports = App;
