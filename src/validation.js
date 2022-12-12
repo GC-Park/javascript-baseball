bb9944861563a7103ea905d71029374af2947f99
GC-Park <kuyt3122@naver.com>
2022-12-12 16:37:07 +0900

    refactor:예외 처리 부분 string을 constant로 변경

--- src/validation.js
+++ src/validation.js
@@ -1,16 +1,18 @@
+const { EXCEPTION } = require("./constant")
+
 const userNumberInputException = (userInput)=>{
-  if (userInput.length !== 3) throw new Error("입력 값이 잘못 되었습니다. 게임을 종료합니다.")
+  if (userInput.length !== 3) throw new Error(EXCEPTION)
 
   let checkString=0
   userInput.map((user) => {
       if (isNaN(user)) checkString=1;
   })
-  if(checkString===1) throw new Error("입력 값이 잘못 되었습니다. 게임을 종료합니다.")
+  if(checkString===1) throw new Error(EXCEPTION)
 
-  if (userInput.includes('0')) throw new Error("입력 값이 잘못 되었습니다. 게임을 종료합니다.")
+  if (userInput.includes('0')) throw new Error(EXCEPTION)
 
   const repeatCheck = new Set(userInput)
-  if (repeatCheck.size !== 3) throw new Error("입력 값이 잘못 되었습니다. 게임을 종료합니다.")
+  if (repeatCheck.size !== 3) throw new Error(EXCEPTION)
 
 }
 
